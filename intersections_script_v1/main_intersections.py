3d66d0ef03c419e77956c553680bbc4c976ecafd
DmitryKutsev <31938190+DmitryKutsev@users.noreply.github.com>
2020-02-06 13:26:24 +0300

    optimized main_intersections.py

--- intersections_script_v1/main_intersections.py
+++ intersections_script_v1/main_intersections.py
@@ -5,28 +5,31 @@ from pymorphy2 import MorphAnalyzer
 import json
 morph = MorphAnalyzer()
 
-#morph.parse(word.replace(':', ''))[0].normal_form
 class Intersector:
+    """
+    Пересекает корпус, разбитый по небольшим файлам(если файлы будут большие - все грохнется),
+    с основным лексиконом. Пересекает только глаголы, считает их кол-во в тексте, возвращает
+    словарь "глагол-количество в тексте".
+    """
     def __init__(self):
         corp_fold = open('main_intersections_config.json', 'r', encoding='utf-8')
         self.corp_fold = json.load(corp_fold, encoding="utf-8")["corpus"]
 
     def norm_tokenize_words(self, text):
-        normalized_text = [word.strip(punctuation) for word in text.lower().split(" ")]
-        normalized_text = [word for word in normalized_text if word]
+        normalized_text = [word.strip(punctuation) for word in text.lower().split(" ") if word.strip(punctuation)]
+        #normalized_text = [word for word in normalized_text if word]
         return normalized_text
 
     def get_lexicon(self):
-        dict_list = []
+        collection_dict = Counter()
         collection_handler = open("collection.json", "r", encoding="utf-8")
         collection = json.load(collection_handler, encoding="utf-8")
         print(collection)
         for wordlist in collection.keys():
             for words in collection[wordlist]['variants']:
-                if len(words.split(" ")) == 1 and morph.parse(words)[0].tag.POS == "INFN":
-                    #print(morph.parse(words.replace(':', ''))[0].normal_form)
-                    dict_list.append(morph.parse(words.replace(':', ''))[0].normal_form)
-        collection_dict = Counter(dict_list)
+                morphy = morph.parse(words)[0]
+                if len(words.split(" ")) == 1 and morphy.tag.POS == "INFN":
+                    collection_dict[morphy.normal_form] += 1
         print(len(collection_dict))
         return collection_dict
 
@@ -38,19 +41,14 @@ class Intersector:
             for filename in filenames:
                 path = os.path.join(dirname, filename)
                 print(path)
-                content_handl = open(path, "r", encoding='utf-8')
-                content = content_handl.read().split(".")
-                content_handl.close()
-                for index in range(len(content)-1):
-                #for sent in my_sent_dict.keys():
-                    sent = content[index]
-                    for word in self.norm_tokenize_words(sent):
-                        norm_form = morph.parse(word.replace(':', ''))[0].normal_form
-                        # if norm_form in my_lexicon and sent != my_list[index - 1]:
-                        if norm_form in my_lexicon:
-                            #print(sent)
-                            #print(word)
-                            words_count[norm_form] += 1
+                content_handl = open(path, "r", encoding='utf-8').read()
+                content = self.norm_tokenize_words(content_handl)
+                for word in content:
+                    norm_form = morph.parse(word.replace(':', ''))[0].normal_form
+                    if norm_form in my_lexicon:
+                        #print(sent)
+                        #print(word)
+                        words_count[norm_form] += 1
         print(words_count)
         with open(corpus_fold + '_result.txt', "w", encoding="utf-8") as writer:
             writer.write(str(words_count))
