17ddb014b6df27be26a2f44e7806bc6d54d3ca73
DmitryKutsev <31938190+DmitryKutsev@users.noreply.github.com>
2020-02-14 02:06:21 +0300

    csv outp added

--- annotations/annotations_handle.py
+++ annotations/annotations_handle.py
@@ -1,10 +1,10 @@
 import os
 from collections import Counter
 import json
-masha = open('annotations/annotation_masha.csv', 'r')
-olya = open('annotations/annotation_masha.csv', 'r')
-grisha = open('annotations/annotation_masha.csv', 'r')
-lesha = open('annotations/annotation_masha.csv', 'r')
+masha = open('annotation_masha.csv', 'r')
+olya = open('annotation_masha.csv', 'r')
+grisha = open('annotation_masha.csv', 'r')
+lesha = open('annotation_masha.csv', 'r')
 
 masha_set = set()
 olya_set= set()
@@ -28,17 +28,24 @@ second_iter = lesha_set.intersection(grisha_set)
 
 final_set = first_iter.intersection(second_iter)
 final_count = Counter()
+import csv
+csvfile =  open('annotations_result.csv', 'w', newline='')
+
+
 for i in final_set:
     try:
         if int(i.split(',')[-1]) == 1:
             print(i)
-            final_count[i] += 1
+            final_count[i.split(',')[0]] = i.split(',')[-1]
+            csv_writer = csv.writer(csvfile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([i])
     except Exception as e:
         print(e.args)
 print(len(final_count))
 with open('annotations_result.json', "w", encoding="utf-8") as writer:
     json.dump(Counter(final_count), writer, ensure_ascii=False)
     writer.close()
+csvfile.close()
 
 masha.close()
 olya.close()
